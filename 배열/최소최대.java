f133c33d1f870b2a54c9c1dfb7f31ba8732f0e24
SeoHoehyeong <86947580+SeoHoeHyeong@users.noreply.github.com>
2021-11-01 23:38:24 +0900

    #3

---
+++ 배열/최소최대.java
@@ -0,0 +1,36 @@
+package code11_01;
+
+import java.util.Scanner;
+
+public class 최소최대 {
+	
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		
+		int N = input.nextInt();
+		
+		int [] arr = new int [N];
+		
+
+		
+		int X ;
+		for(int i = 0; i < N; i++) {
+			arr[i] = input.nextInt();
+		}
+		int max = arr[0];
+		
+		int min = arr[0];
+		
+		for(int i = 0; i < N; i++) {
+			if(max <= arr[i]) {
+				max = arr[i];
+			}
+			else if(min >= arr[i]) {
+				min = arr[i];
+			}
+		}
+		System.out.println(min + " " + max);
+	}
+}
